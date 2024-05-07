1b35515935e2574046433c0be452ccca33013945
DrOwen101 <151655844+DrOwen101@users.noreply.github.com>
2024-05-07 13:38:20 -0500

    branching test push

--- Jason Owen/FirstJavaClass.java
+++ Jason Owen/FirstJavaClass.java
@@ -1,5 +1,6 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
+        //Comment your code often
         System.out.println("I just made my first Java Class in VSCode");
     }
 }
