606bc3e5f906ade7f4ba26a0cd4b151559eb12b1
DrOwen101 <151655844+DrOwen101@users.noreply.github.com>
2024-05-07 13:44:47 -0500

    merge me

--- Jason Owen/FirstJavaClass.java
+++ Jason Owen/FirstJavaClass.java
@@ -1,6 +1,6 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
-        //Comment your code often
+        //Comment your code often and well
         System.out.println("I just made my first Java Class in VSCode");
     }
 }
